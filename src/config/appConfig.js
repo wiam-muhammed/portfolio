c0e5dfab8ae8693dd18a98440696baf58da6417c
wiam-muhammed <weam.mustafa01@gmail.com>
2025-12-30 17:35:18 +0200

    add logo

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -1,6 +1,6 @@
 export default {
   siteName: "WM",
-  siteLogo: "/logo.svg",
+  siteLogo: "/portfolio/data/images/logo.svg",
 
   userName: "Hi, I'm Weam.",
   userTitle: "Translator & Language Specialist",
